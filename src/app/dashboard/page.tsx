9928a00eda8db0496343996e8ef4aefa6362088f
Bernhard Chepete <chepetebernhard8@gmail.com>
2025-12-22 07:48:14 +0000

    No , I only get to see the representative charts when I click on one of

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,23 @@
+'use client';
+
 import { DashboardHeader } from "@/components/dashboard-header";
 import {
   OverviewCards,
   PerformanceChartCard,
-  AIInsightsCard
+  AIInsightsCard,
 } from "./cards";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col h-full">
       <DashboardHeader title="Dashboard" />
-      <main className="flex-1 p-4 sm:p-6">
+      <main className="flex-1 p-4 sm:p-6 overflow-y-auto">
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
           <OverviewCards />
-          <div className="lg:col-span-4">
+        </div>
+        <div className="grid gap-6 mt-6">
             <AIInsightsCard />
-          </div>
-          <div className="lg:col-span-4">
             <PerformanceChartCard />
-          </div>
         </div>
       </main>
     </div>
